0d1a18f51fa23c415a8c378dbfc1dac9cc12a1ec
ilmoro90 <daniele_moriggi@hotmail.it>
2021-05-29 09:52:34 +0200

    funzionante

--- ProgettoSanitario/strutturadialog.cpp
+++ ProgettoSanitario/strutturadialog.cpp
@@ -12,36 +12,3 @@ strutturaDialog::~strutturaDialog()
 {
     delete ui;
 }
-
-void strutturaDialog::on_lineNomeStruttura_textChanged(const QString &arg1)
-{
-    this->aux = arg1;
-}
-
-
-
-void strutturaDialog::on_gradiSpinBox_valueChanged(int arg1)
-{
-    this->gradi=arg1;
-}
-
-
-void strutturaDialog::on_minutiSpinBox_valueChanged(int arg1)
-{
-    this->minuti=arg1;
-}
-
-
-void strutturaDialog::on_secondiSpinBox_valueChanged(int arg1)
-{
-    this->secondi=arg1;
-}
-
-
-
-
-void strutturaDialog::on_pushButton_inserisci_clicked()
-{
-
-}
-
--- ProgettoSanitario/strutturadialog.h
+++ ProgettoSanitario/strutturadialog.h
@@ -15,19 +15,6 @@ public:
     explicit strutturaDialog(QWidget *parent = nullptr);
     ~strutturaDialog();
 
-private slots:
-    void on_lineNomeStruttura_textChanged(const QString &arg1);
-    QString aux;
-    int gradi,minuti,secondi;
-
-    void on_gradiSpinBox_valueChanged(int arg1);
-
-    void on_minutiSpinBox_valueChanged(int arg1);
-
-    void on_secondiSpinBox_valueChanged(int arg1);
-
-    void on_pushButton_inserisci_clicked();
-
 private:
     Ui::strutturaDialog *ui;
 };
